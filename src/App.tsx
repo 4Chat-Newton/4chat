4785e96d15d7bf0cb7997ff93daf7ea04abe570e
barkatrahimi <92032286+barkatrahimi@users.noreply.github.com>
2023-01-23 15:23:21 +0100

    registration form
    logo fixed on both login and signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SignUp from './formComponent';
+import Login from './registerForm';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header >
 
         <SignUp></SignUp>
+        <Login></Login>
 
       </header>
     </div>
