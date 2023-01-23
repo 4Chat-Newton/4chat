a9e70a89a0083a5b1e94bb92d8e039699ae0f524
barkatrahimi <92032286+barkatrahimi@users.noreply.github.com>
2023-01-23 15:23:21 +0100

    registration form
    in progress

--- src/index.tsx
+++ src/index.tsx
@@ -9,5 +9,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    
   </React.StrictMode>
 );
--- src/registerForm.tsx
+++ src/registerForm.tsx
@@ -18,7 +18,7 @@ import './style.css'
 */
 import { LockClosedIcon } from '@heroicons/react/20/solid'
 
-export default function Login() {
+export default function SignUp() {
   return (
     <>
       {/*
@@ -32,14 +32,15 @@ export default function Login() {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
-           {/*  <img
+            {/* <img
               className="mx-auto h-12 w-auto"
-              src=""
+              src="./assets/4chat.png"
               //src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt="Your Company"
             /> */}
+            
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Sign in to your account
+                Create a new account
             </h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               
@@ -77,6 +78,18 @@ export default function Login() {
                   required
                   className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   placeholder="Password"
+                />
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Confirm Password"
                 />
               </div>
             </div>
