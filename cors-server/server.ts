2f8f842d8e94db03f5124da36ddebcb456bc527d
sami <sami@gmail.dk>
2023-01-24 14:30:55 +0100

    added password and password validation  bcrypt

--- cors-server/server.ts
+++ cors-server/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
-const server:any = express();
+import {encryptPassword, validateUser} from './authentication';
+const server = express();
 const port:Number = 8080;
 
 server.get('/', (req, res) => {
@@ -9,3 +10,9 @@ server.get('/', (req, res) => {
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+const myPlaintextPassword = 'Hannes420';//Ska laddas från db
+server.get('/auth', async ()=>{
+    await encryptPassword(myPlaintextPassword)
+})
+
