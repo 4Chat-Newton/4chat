46a4df2886a679d178c97c8dbe182c47aed8813c
M0ller <simon.mollerberg@gmail.com>
2023-01-24 15:34:09 +0100

    added post to database

--- cors-server/server.ts
+++ cors-server/server.ts
@@ -10,6 +10,9 @@ export const db =  new sqlite.Database('database.db', (err) => {
     console.log('Connected to SQlite database.')
 });
 
+server.use(express.json());
+server.use(express.urlencoded())
+
 server.get('/', (req, res) => {
     res.send('NodeJS + Express + Typescript App Up! 👍');
 });
@@ -31,11 +34,12 @@ server.get('/register', (req, res) => {
         }
     })
 })
-server.post('/register', async (request: express.Request, response: express.Response) => {
-    const {name} = request.body;
+server.post('/register', (request: express.Request, response: express.Response) => { // express.Request
+    const { username, email, password, online } = request.body;
     console.log(request.body)
-    console.log(name)
-    const query = 'INSERT INTO user (username) VALUES(?)';
-    db.run(query, name);
-    // response.json({ userCreated: true });
+    console.log(username, email, password, online)
+    const query = 'INSERT INTO user (username, email, password, online) VALUES(?, ?, ?, ?)';
+    db.run(query, [username, email, password, online]);
+    response.json({ userCreated: true });
 });
+
