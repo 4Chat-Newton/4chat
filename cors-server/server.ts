4d3c013fb4a7d75212be5884fed32c198d2e3833
M0ller <simon.mollerberg@gmail.com>
2023-01-24 15:11:52 +0100

    work in progress

--- cors-server/server.ts
+++ cors-server/server.ts
@@ -1,15 +1,41 @@
 import express from "express";
-const server:any = express();
+import sqlite from "sqlite3";
+import {useCallback} from "react";
+export const server:any = express();
 const port:Number = 8080;
+export const db =  new sqlite.Database('database.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to SQlite database.')
+});
 
 server.get('/', (req, res) => {
     res.send('NodeJS + Express + Typescript App Up! 👍');
 });
 
+
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
+
 server.get('/register', (req, res) => {
 
+    db.all("SELECT * FROM user", (err, rows) => {
+        if (err) {
+            // console.log(err);
+        } else {
+            res.send(rows);
+            // console.log(rows);
+        }
+    })
 })
+server.post('/register', async (request: express.Request, response: express.Response) => {
+    const {name} = request.body;
+    console.log(request.body)
+    console.log(name)
+    const query = 'INSERT INTO user (username) VALUES(?)';
+    db.run(query, name);
+    // response.json({ userCreated: true });
+});
