5c648c445e2cf167037867ec5e3e901294868997
sami <sami@gmail.dk>
2023-01-25 10:26:44 +0100

    added accessors and notes for methods

--- src/Types/User.ts
+++ src/Types/User.ts
@@ -2,34 +2,106 @@ import { any, number, string } from "prop-types";
 import { Report } from "./Report";
 import { Room } from "./Room";
 
- export class User {
-     private username: string;
-     private password: number;
-     private email: string;
-     private friends: Array<User>;
-     private createdRooms: Array<Room>;
-     private  moderatedRooms: Array<Room>;
-     private reports: Array<Report>;
-     private blockedUsers: Array<User>;    
-     constructor(
-         username: string, 
-         password: number, 
-         email: string, 
-         friends: Array<User>, 
-         createdRooms: Array<Room>, 
-         moderatedRooms: Array<Room>, 
-         reports: Array<Report>,
-         blockedUsers: Array<User>
-         ){
-         this.username = username;
-         this.password= password;
-         this.email= email;
-         this.friends = friends;
-         this.createdRooms = createdRooms;
-         this.moderatedRooms = moderatedRooms;
-         this.reports= reports;
-         this.blockedUsers= blockedUsers;
-     }
+export class User {
+    private _username: string;
+    private _password: number;
 
+    private _email: string;
 
- }
+    private _friends: Array<User>;
+
+    private _createdRooms: Array<Room>;
+
+    private _moderatedRooms: Array<Room>;
+
+    private _reports: Array<Report>;
+    private _blockedUsers: Array<User>;
+
+    constructor(
+        username: string,
+        password: number,
+        email: string,
+        friends: Array<User>,
+        createdRooms: Array<Room>,
+        moderatedRooms: Array<Room>,
+        reports: Array<Report>,
+        blockedUsers: Array<User>
+    ) {
+        this._username = username;
+        this._password = password;
+        this._email = email;
+        this._friends = friends;
+        this._createdRooms = createdRooms;
+        this._moderatedRooms = moderatedRooms;
+        this._reports = reports;
+        this._blockedUsers = blockedUsers;
+    }
+    //------------ Methods ---------------
+
+    // AddFriend:void = (friendId:number)=>{
+    //     //? DB logik
+    //     let friend:User = findUser(friendId)
+    //     this._friends.push(friend)
+    // }
+    // FindUser:User = (userId:number)=>{
+    //     return User where id matches the number
+    // }
+
+    //------------ Accessors -------------
+
+    public get username(): string {
+        return this.username;
+    }
+    public set username(value: string) {
+        this.username = value;
+    }
+
+    public get password(): number {
+        return this._password;
+    }
+    public set password(value: number) {
+        this._password = value;
+    }
+
+    public get email(): string {
+        return this._email;
+    }
+    public set email(value: string) {
+        this._email = value;
+    }
+
+    public get friends(): Array<User> {
+        return this._friends;
+    }
+    public set friends(value: Array<User>) {
+        this._friends = value;
+    }
+
+    public get createdRooms(): Array<Room> {
+        return this._createdRooms;
+    }
+    public set createdRooms(value: Array<Room>) {
+        this._createdRooms = value;
+    }
+
+    public get moderatedRooms(): Array<Room> {
+        return this._moderatedRooms;
+    }
+    public set moderatedRooms(value: Array<Room>) {
+        this._moderatedRooms = value;
+    }
+
+    public get reports(): Array<Report> {
+        return this._reports;
+    }
+    public set reports(value: Array<Report>) {
+        this._reports = value;
+    }
+
+    public get blockedUsers(): Array<User> {
+        return this._blockedUsers;
+    }
+    public set blockedUsers(value: Array<User>) {
+        this._blockedUsers = value;
+    }
+}
