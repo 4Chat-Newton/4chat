701041fae49356077d024e358301dcc731fa2e2e
Yusupha Jow <Jooow_@live.se>
2023-01-25 15:41:06 +0100

    Routes fixade

--- src/components/ChatRouter.tsx
+++ src/components/ChatRouter.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import App from "../App";
+import Login from "./Login";
+import Register from "./Register"
 
 function ChatRouter() {
 
     return <>
     <Routes>
         <Route path="/" element={ <App /> } />
+        <Route path="/login" element={ <Login /> } />
+        <Route path="/register" element={ <Register /> } />
     </Routes>
     </>
 
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -13,9 +13,6 @@ export default function Login() {
               alt="Logo"
               className="mx-auto h-20 w-auto"
             />
-{/*             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Sign in to your account
-            </h2> */}
             <p className="mt-2 text-center text-sm text-gray-600">
               <a
                 href="#"
@@ -56,18 +53,6 @@ export default function Login() {
               </div>
             </div>
             <div>
-{/*               <button
-                type="submit"
-                className="text-blue-500 bg-gray-700 group relative flex w-full justify-center rounded-md border border-transparent py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <LockClosedIcon
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                    aria-hidden="true"
-                  />
-                </span>
-                Sign in
-              </button> */}
               <button className="bg-gray-700 px-7 py-2 text-blue-700 ml-40" type="submit">Login</button>
             </div>
             <div className="text-sm">
