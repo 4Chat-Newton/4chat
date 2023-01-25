79d1ef2c44aecc4fbe1b87f8f0ec41712ba9a9a1
Simon M <65451384+M0ller@users.noreply.github.com>
2023-01-25 15:46:38 +0100

    Update Register.tsx
    removed comments

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -9,9 +9,6 @@ export default function SignUp() {
 
             <img src="img/4chat.png"className="mx-auto h-20 w-auto" alt="logo" />
             
-{/*             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                Create a new account
-            </h2> */}
             <p className="mt-2 text-center text-sm text-gray-600">
               
               <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
