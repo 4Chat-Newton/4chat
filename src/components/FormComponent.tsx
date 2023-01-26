30a53ef04e317950f3fd958ee7269457ba1c4a3d
barkatrahimi <92032286+barkatrahimi@users.noreply.github.com>
2023-01-26 09:59:04 +0100

    Email validation
    fixed a tag and created terms and conditions page

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -8,23 +8,27 @@ export default function Login() {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
-          <a
-                href="/"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
             <img
               src="img/4chat.png"
               alt="Logo"
               className="mx-auto h-20 w-auto"
             />
-            </a>
+{/*             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2> */}
+            <p className="mt-2 text-center text-sm text-gray-600">
+              <a
+                href="/register"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              ></a>
+            </p>
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="-space-y-px rounded-md shadow-sm">
               <div>
                 <label htmlFor="email-address" className="sr-only">
-                  email
+                  Email address
                 </label>
                 <input
                   id="email-address"
@@ -33,7 +37,7 @@ export default function Login() {
                   autoComplete="email"
                   required
                   className="mb-3 relative block w-full appearance-none rounded-none rounded-t-md border border-none px-3 py-2 text-lime-400 placeholder-lime-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="email"
+                  placeholder="Email address"
                 />
               </div>
               <div>
@@ -47,17 +51,56 @@ export default function Login() {
                   autoComplete="current-password"
                   required
                   className="relative block w-full appearance-none rounded-none rounded-b-md border border-none px-3 py-2 text-yellow-400 placeholder-yellow-300 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="password"
+                  placeholder="Password"
                 />
               </div>
             </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
             <div>
+{/*               <button
+                type="submit"
+                className="text-blue-500 bg-gray-700 group relative flex w-full justify-center rounded-md border border-transparent py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  />
+                </span>
+                Sign in
+              </button> */}
               <button className="bg-gray-700 px-7 py-2 text-blue-700 ml-40" type="submit">Login</button>
             </div>
             <div className="text-sm">
                 <a
-                  href="/register"
-                  className="px-7 py-2 text-blue-700 ml-40 bg-transparent text-decoration-line: underline"
+                  href="#"
+                  className="px-7 py-2 text-blue-700 ml-40 bg-transparent"
                 >
                   Register
                 </a>
