097a744af6f14508a429c45349521151d7f09239
M0ller <simon.mollerberg@gmail.com>
2023-01-26 10:02:30 +0100

    swapped places

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -19,6 +19,22 @@ export default function SignUp() {
           <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="-space-y-px rounded-md shadow-sm">
+
+              <div>
+                <label htmlFor="user-name" className="sr-only">
+                  User Name
+                </label>
+                <input
+                    id="user-name"
+                    name="username"
+                    type="username"
+                    autoComplete="username"
+                    required
+                    className="mb-3 relative block w-full appearance-none rounded-none rounded-t-md border border-none px-3 py-2 text-lime-400 placeholder-lime-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="username"
+                />
+              </div>
+
               <div>
                 <label htmlFor="email-address" className="sr-only">
                   Email address
@@ -33,20 +49,7 @@ export default function SignUp() {
                   placeholder="email"
                 />
               </div>
-              <div>
-                <label htmlFor="user-name" className="sr-only">
-                  User Name
-                </label>
-                <input
-                  id="user-name"
-                  name="username"
-                  type="username"
-                  autoComplete="username"
-                  required
-                  className="mb-3 relative block w-full appearance-none rounded-none rounded-t-md border border-none px-3 py-2 text-lime-400 placeholder-lime-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="username"
-                />
-              </div>
+
               <div>
                 <label htmlFor="password" className="sr-only">
                   Password
