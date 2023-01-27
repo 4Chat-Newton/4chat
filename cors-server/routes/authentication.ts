04aba35fc01c90f7dfb26fa3e386f8081fbbffd7
sami <sami@gmail.dk>
2023-01-27 14:02:32 +0100

    resolved comments

--- cors-server/routes/authentication.ts
+++ cors-server/routes/authentication.ts
@@ -1,8 +1,8 @@
 import bcrypt from "bcrypt";
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import session from "express-session"
-import {server} from "../server";
+import session from "express-session";
+import { server } from "../server";
 
 export const encryptPassword = async function (password: string) {
     let saltRounds = await bcrypt.genSalt(11);
@@ -11,8 +11,6 @@ export const encryptPassword = async function (password: string) {
 };
 
 export const validateUser = async function (password: string, hash: string) {
-    console.log("r14 password ", password," hash ", hash);
-    
     let success: boolean = await bcrypt.compare(password, hash);
     return success;
 };
@@ -20,9 +18,10 @@ export const validateUser = async function (password: string, hash: string) {
 export const findUser = async function (email, db) {
     try {
         const result = db
-            .prepare("SELECT id, email, username, password FROM user WHERE email = ?")
+            .prepare(
+                "SELECT id, email, username, password FROM user WHERE email = ?"
+            )
             .get(email);
-        console.log(result.id, result.username, result.email);
 
         if (!result) {
             console.log(`No user found with email ${email}`);
@@ -35,8 +34,6 @@ export const findUser = async function (email, db) {
     }
 };
 
-
-
 export const signIn = async function (server, db: any, newLogin: boolean) {
     if (newLogin) {
         server.post("/data/login", async (req: Request, res: Response) => {
@@ -45,42 +42,36 @@ export const signIn = async function (server, db: any, newLogin: boolean) {
             // Verify the user's credentials against the database
             try {
                 const user = await findUser(email, db);
-                console.log("r49",user);
-                
+
                 if (!user) {
                     return res
                         .status(401)
-                        .json({ error: "r54 Invalid username or password.." });
+                        .json({ error: "Invalid username or password.." });
                 }
-                console.log("r56", user);
 
                 const isValid = await validateUser(password, user.password);
-                
+
                 if (!isValid) {
                     return res
                         .status(401)
                         .json({ error: "Invalid username or password.." });
                 }
-                console.log("r66", user)
                 // Create a new JWT with the user's information
                 const payload = { id: user.id };
                 const options = {
                     expiresIn: "2h",
                 };
-                const secret_key = "secret_key"
-                
+                const secret_key = "secret_key";
+
                 const token = jwt.sign(payload, secret_key, options);
-                console.log("r73 token ", token);
 
                 // Store the JWT in the user's session
-                // req.body.session.jwt = token;
                 user.password = undefined;
                 res.cookie("token", token, {
                     httpOnly: true,
-                    //secure: true, // only works on https    
+                    //secure: true, // only works on https
                 });
-                console.log("r80", req.header);
-                
+
                 res.status(200).json({ loggedIn: true });
 
                 return res.json(user);
@@ -88,7 +79,7 @@ export const signIn = async function (server, db: any, newLogin: boolean) {
                 // delete req.body.session.jwt;
                 return res
                     .status(500)
-                    .json({ error: "R.129 Internal server error" });
+                    .json({ error: "Internal server error" });
             }
         });
     } else {
