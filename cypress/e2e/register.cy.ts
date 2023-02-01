150a378deea5dcd0225a1e62f6e5aa3046ab13ff
Simon M <65451384+M0ller@users.noreply.github.com>
2023-02-01 13:02:54 +0100

    Update register.cy.ts
    removed comments

--- cypress/e2e/register.cy.ts
+++ cypress/e2e/register.cy.ts
@@ -72,37 +72,3 @@ describe('Testing user functionality', () => {
     })
 
 })
-
-// describe('Testing 2', () => {
-//
-//     it('Login a user', () => {
-//
-//         cy.visit('http://localhost:3000/login')
-//         cy.intercept("POST", "/data/login", (req) => {
-//             req.continue((res) => {
-//                 expect(res.statusCode).to.eq(200)
-//                 //expect(res.body.loggedIn).to.have.eq(true)
-//             })
-//         })
-//
-//         cy.wait(2000)
-//
-//         cy.get('#email').click().type("Test_User@gmail.com")
-//         cy.get('#password').click().type("12345")
-//         cy.get('#login_btn').click()
-//
-//
-//     })
-// })
-//
-// describe('Testing 3', () => {
-//
-//     it('Delete a user', () => {
-//         // TODO add intercept or something else after the initial create user have been created
-//         cy.wait(2000)
-//         cy.request('DELETE', 'http://localhost:8080/data/register/Test_User').then((response) => {
-//             expect(response.status).to.eq(200)
-//         })
-//     })
-//
-// })
