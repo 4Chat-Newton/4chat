c83766bdf367094e886c180adfc6092588c78bdf
Hannes Westin <hannes.westin@edu.newton.se>
2023-02-01 16:04:37 +0100

    Overflow fix + scrollbar

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -6,62 +6,13 @@ export default function Chat(){
     <div className="chatWindow">
         <section className="roomName">
           <div className="roomName-header bg-blue-700">
-                <h1>test</h1>
+                <h1>#Room</h1>
           </div>
-
-
         </section>
         <section className="chat">
             <ul className="text">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.imilique autem placeat adipisci voluptatem nam quibusdam?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quos. Eligendi eum tempore earum ipsam totam dolorum quae ha</p>
-                
-
+                {Array(50).fill(null).map(() => (<p>oi</p>))}
             </ul>
         </section>
         
