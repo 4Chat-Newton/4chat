121412eb96fac2f24e8888d7af3f02ed3714664f
M0ller <simon.mollerberg@gmail.com>
2023-02-02 12:58:36 +0100

    testing new script

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 // import { useEffect, useState } from 'react';
+import {Link} from "react-router-dom";
 
 function App() {
 
 
-
-  return (
-    <div className='App' style={ { display: "flex", flexDirection: "column", color: "wheat"}}>
-      <header >
-        <h1 style={{textAlign: "center"}} >Hello World</h1>
-      </header>
-        <div style={{
-            display: "flex",
-            justifyContent: "center",
-            color: "wheat"
-        }}>
+    return (
+        <div className='App' style={{display: "flex", flexDirection: "column", alignItems: "center", color: "wheat"}}>
+            <header>
+                <h1>Hello 4chat</h1>
+            </header>
+            <span> <br/> </span>
+            <Link to={"/login"}> Login page</Link>
+            <Link to={"/register"}> Register page</Link>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
