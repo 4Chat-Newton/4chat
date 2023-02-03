c9d4e404f8b3e306a0e7c2839be592cfa2ab86b5
barkatrahimi <92032286+barkatrahimi@users.noreply.github.com>
2023-02-03 11:34:30 +0100

    css color
    login register field css color fixed

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -67,7 +67,7 @@ export default function Login() {
                                 type="email"
                                 autoComplete="email"
                                 required
-                                className="mb-3 relative block w-full appearance-none rounded-none rounded-t-md border border-none px-3 py-2 text-lime-400 placeholder-lime-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                className="bg-gray-700 mb-3 relative block w-full appearance-none rounded-none rounded-t-md border border-none px-3 py-2 text-lime-400 placeholder-lime-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                 placeholder="email"
                                 onChange={(e) => handleUserInput(e)}
                             />
