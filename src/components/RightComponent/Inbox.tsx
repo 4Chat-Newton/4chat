bfe4848a8ffa40a9ed3f032f74da5aa41de03686
sami <sami@gmail.dk>
2023-02-06 15:54:17 +0100

    Revert "fixad css"
    This reverts commit eab8207a1ad51be65f4e2d63331796e2c4c75726.

--- src/components/RightComponent/Inbox.tsx
+++ src/components/RightComponent/Inbox.tsx
@@ -1,20 +1,13 @@
-import { useState } from "react";
 import ButtonComponent from "../GlobalComponents/ButtonComponent";
 import "./Inbox.css";
 
 export default function Inbox() {
 
-  const [isActive, setActive] = useState(true);
-  const toggleClass = () => {
-    setActive(!isActive);
-  }
-
-
   return (
     <>
       <section className="inbox">
         <div className="inboxBtns">
-      <ButtonComponent onClick={()=> setActive(!isActive)} className={isActive ? "active" : null}>Inbox</ButtonComponent>
+          <ButtonComponent className="inboxBtn">Inbox</ButtonComponent>
           <ButtonComponent className="roomsBtn">Rooms</ButtonComponent>
         </div>
       </section>
