5dcc16373687c94fc44e4413dbeefe088ff2c252
barkatrahimi <92032286+barkatrahimi@users.noreply.github.com>
2023-02-06 14:06:58 +0100

    Revert "Merge remote-tracking branch 'origin/dev' into GG-98-create-UI-for-chatrooms"
    This reverts commit 9c7794e1b4518521f5ba59adbbe54d62447ed0df, reversing
    changes made to 3f0309b5b5cd7490a0463112e87aea54d3043ec6.

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -2,27 +2,14 @@ import ChatMessage from "./ChatMessage";
 import ChatWindow from "./ChatWindow";
 import "../style.css";
 import BoxContainer from "../GlobalComponents/BoxContainer";
-import { io } from "socket.io-client";
-import { useEffect, useState } from "react";
 
-const socket = io("http://localhost:8080");
-
-
-
-const Chat = () => {
-  const [messages, setMessages] = useState<any>([]);
-
-  useEffect(() => {
-    socket.on('messageResponse', (data) => setMessages([...messages, data]));
-  }, [socket, messages]);
-
+export default function Chat() {
   return (
     <>
-    <BoxContainer className="chatContainer">
-        <ChatWindow messages={messages}/>
-        <ChatMessage socket={socket}/>
+      <BoxContainer className="chatContainer">
+        <ChatWindow />
+        <ChatMessage />
       </BoxContainer>
     </>
   );
 }
-export default Chat;
