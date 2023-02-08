36c027b722a03c65616d208f2abab0271939e13c
barkatrahimi <92032286+barkatrahimi@users.noreply.github.com>
2023-02-08 14:44:21 +0100

    login navigate fixed logout in progress

--- src/components/GlobalComponents/Navbar.tsx
+++ src/components/GlobalComponents/Navbar.tsx
@@ -1,7 +1,20 @@
 import ButtonComponent from "./ButtonComponent"
 import "./Navbar.css"
+import { useNavigate } from "react-router";
 
 export default function Navbar() {
+
+  const navigate = useNavigate();
+
+  const handleSignout = async () => {
+    //TODO fetch should be '/data/login'
+    await fetch('http://localhost:8080/data/login', {
+        method: 'DELETE'
+    })
+    navigate("/login");
+    }
+
+
   return <>
 
     <header className="navbar-header">
@@ -17,7 +30,7 @@ export default function Navbar() {
 
       <div className="nav-btn">
         <ButtonComponent className="settings-btn">Settings</ButtonComponent>
-        <ButtonComponent className="signOut-btn">Sign out</ButtonComponent>
+        <ButtonComponent className="signOut-btn" onClick={handleSignout}>Sign out</ButtonComponent>
       </div>
     </header>
   </>
