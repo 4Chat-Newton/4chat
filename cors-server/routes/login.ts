f433f4b601e16deb343972bf5e6682f06925e462
M0ller <simon.mollerberg@gmail.com>
2023-02-08 12:01:45 +0100

    save

--- cors-server/routes/login.ts
+++ cors-server/routes/login.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import {Request, Response} from "express";
 import jwt from "jsonwebtoken";
 import cookieparser from "cookie-parser";
 import {findUser, requireSignin, validateUser} from "../controllers/authentication";
@@ -90,38 +90,15 @@ export const signIn = async function (server, db: any) {
         status: false
     }
     // const isLoggedIn = db.prepare("SELECT online FROM user WHERE id = ?")
-    server.get("/data/login", async (req: Request, res: Response) => {
+    server.post("/data/login", async (req: Request, res: Response) => {
         isLoggedIn = requireSignin(req, res)
-        // Extract the user's credentials from the request body
 
-        console.log("req.body: ", req.body)
-        //TODO check token this
-        console.log("isLoggedIn: ", isLoggedIn.status)
-        console.log("isLoggedIn: ", isLoggedIn.id)
-        console.log("req.cookies.token: ", req.cookies.token)
+        console.log("isLoggedIn.status: ", isLoggedIn.status)
+        console.log("isLoggedIn.id: ", isLoggedIn.id)
+        if (!isLoggedIn.status && isLoggedIn.id !== null) {
 
-
-
-        //! Check if JWT cookie exists
-        if (req.cookies.token) {
-            try {
-                const data = jwt.verify(req.cookies.token, "secret_key");
-
-                return res.status(200).json({ loggedIn: true, data });
-            } catch (err) {
-                return res.status(401).json({ error: "Invalid token" });
-            }
-        } else {
-            return res.status(401).json({ error: "No active session" });
-        }
-    });
-
-    if (!isLoggedIn) {
-
-    } else {
-        server.post("/data/login", async (req: Request, res: Response) => {
             // Extract the user's credentials from the request body
-            const { email, password } = req.body;
+            const {email, password} = req.body;
             // Verify the user's credentials against the database
             try {
                 const user = await findUser(email, db);
@@ -129,7 +106,7 @@ export const signIn = async function (server, db: any) {
                 if (!user) {
                     return res
                         .status(401)
-                        .json({ error: "Invalid credentials!" });
+                        .json({error: "Invalid credentials!"});
                 }
 
                 const isValid = await validateUser(password, user.password);
@@ -137,15 +114,15 @@ export const signIn = async function (server, db: any) {
                 if (!isValid) {
                     return res
                         .status(401)
-                        .json({ error: "Invalid credentials!" });
+                        .json({error: "Invalid credentials!"});
                 }
+
                 // Create a new JWT with the user's information
-                const payload = { id: user.id };
+                const payload = {id: user.id};
                 const options = {
                     expiresIn: "2h",
                 };
                 const secret_key = "secret_key";
-
                 const token = jwt.sign(payload, secret_key, options);
 
                 // Store the JWT in the user's session
@@ -158,28 +135,56 @@ export const signIn = async function (server, db: any) {
                 await db.prepare("UPDATE user SET online = 1 WHERE id = ?").run(user.id)
                 // isLoggedIn = true
 
-                return res.status(200).json({ loggedIn: true, user });
+                return res.status(200).json({loggedIn: true, user});
             } catch (err) {
                 // delete req.body.session.jwt;
                 return res
                     .status(500)
-                    .json({ error: "Internal server error" });
+                    .json({error: "Internal server error"});
             }
-        });
 
-    }
+        } else {
+            console.log("User already logged in")
+            return res
+                .status(400)
+                .json({message: "User already logged in", loggedIn: true});
+        }
+
+
+    });
+
+
+        // Extract the user's credentials from the request body
+
+
+
+        // //! Check if JWT cookie exists
+        // if (req.cookies.token) {
+        //     try {
+        //         const data = jwt.verify(req.cookies.token, "secret_key");
+        //
+        //         return res.status(200).json({ loggedIn: true, data });
+        //     } catch (err) {
+        //         return res.status(401).json({ error: "Invalid token" });
+        //     }
+        // } else {
+        //     return res.status(401).json({ error: "No active session" });
+        // }
+
+
 };
 export const signOut = async function (server, db: any) {
-    server.delete('/data/login', async (request, response)=>{
+    server.delete('/data/login', async (request, response) => {
         const isLoggedIn = requireSignin(request, response)
 
-        if (isLoggedIn.status){
+        if (isLoggedIn.status) {
             await db.prepare("UPDATE user SET online = 0 WHERE id = ?").run(isLoggedIn.id)
             response.clearCookie('token')
-            response.status(200).json({ message: `${isLoggedIn.id} is logged out`,  loggedIn: false})
+            response.status(200).json({message: `${isLoggedIn.id} is logged out`, loggedIn: false})
         } else {
-            response.status(400).json({ message: "Something went wrong!"})
+            response.status(400).json({message: "Something went wrong!"})
         }
-    })}
+    })
+}
 
-export default { signIn, signOut };
+export default {signIn, signOut};
