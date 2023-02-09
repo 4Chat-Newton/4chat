cef056f232c111a414f3624f22ee6d42068d97ef
sami <sami@gmail.dk>
2023-02-09 12:39:44 +0100

    appended list to display all available rooms and minor changes to css

--- src/components/RightComponent/Tabs/RoomTab.tsx
+++ src/components/RightComponent/Tabs/RoomTab.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+import ListComponent from "../../GlobalComponents/ListComponent";
 import TabComponent from "../../GlobalComponents/TabComponent";
 
 const RoomTab = () =>{
+    const[rooms, setRooms] = useState([])
+
+    fetch('http://localhost:8080/data/room')
+        .then(res => res.json())
+        .then(data => {
+            // console.log('r10',data)
+            setRooms(data)
+        })
+
+        if(rooms.length > 0) return (<ListComponent rooms={rooms}/>)
 
     return (
         <TabComponent className="RoomTab">
