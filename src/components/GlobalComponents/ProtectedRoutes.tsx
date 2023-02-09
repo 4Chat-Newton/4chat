e966d63bc4978fbe2057f398d34be4bdd4f79931
Hannes Westin <hannes.westin@edu.newton.se>
2023-02-09 15:25:56 +0100

    Possible solution if we end up using localStorage still needs to be tested

--- src/components/GlobalComponents/ProtectedRoutes.tsx
+++ src/components/GlobalComponents/ProtectedRoutes.tsx
@@ -1,11 +1,25 @@
+import { response } from "express";
 import { Outlet, Navigate } from "react-router-dom";
 
 const ProtectedRoutes = () => {
     //TODO actual logic for handling tokens
+    const jwtToken = localStorage.getItem('token') //this could work if we use localstorage
+    // const getToken = async () => {
+    //     await fetch('http://localhost:8080/data/login', {
+    //         method: 'GET',
+    //         headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${jwtToken}` },
+    //     }).then(response => {
+    //         if(jwtToken !==null){
+    //             return true
+    //         } else {
+    //             return false
+    //         }
+    //     })
+    // }
 
-    let auth = {'token': true}
-    return(
-        auth.token ? <Outlet/> : <Navigate to="/" />
+    let auth = { jwtToken }
+    return (
+        auth ? <Outlet /> : <Navigate to="/" />
     )
 };
 
