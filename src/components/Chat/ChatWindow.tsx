57845652bbf1e7637a84e814c7cc5d8f1e6c0334
sami <sami@gmail.dk>
2023-02-09 14:06:51 +0100

    chat scrolls to buttom when new message

--- src/components/Chat/ChatWindow.tsx
+++ src/components/Chat/ChatWindow.tsx
@@ -1,21 +1,40 @@
-import "./Chat.css"
+import "./Chat.css";
+import { useState, useEffect, useRef } from "react";
 
-export default function ChatWindow({messages: socketRespons}: any) {
-    return (<>
-            <div className="chatWindow">
-                <section className="roomName">
-                    <div className="roomName-header">
-                        <h1>#Room</h1>
-                    </div>
-                </section>
-                <section className="chat">
-                    <ul className="text">
-                        {socketRespons.map((msg: any) => (
-                            (<li key={msg.id}>{`[${msg.timeStamp}][${msg.user}]: ${msg.text}`}</li>)
-                        ))}
-                    </ul>
-                </section>
-            </div>
-        </>
-    );
+export default function ChatWindow({ messages: socketRespons }: any) {
+//   const [messages, setMessages] = useState<string[]>([]);
+  const container = useRef<HTMLUListElement>(null);
+
+  const Scroll = () => {
+    const { offsetHeight, scrollHeight, scrollTop } =
+      container.current as HTMLUListElement;
+    if (scrollHeight <= scrollTop + offsetHeight + 100) {
+      container.current?.scrollTo(0, scrollHeight);
+    }
+  };
+
+  useEffect(() => {
+    Scroll()    
+  }, [socketRespons]);
+
+  return (
+    <>
+      <div className="chatWindow">
+        <section className="roomName">
+          <div className="roomName-header">
+            <h1>#Room</h1>
+          </div>
+        </section>
+        <section className="chat" ref={container}>
+          <ul className="text">
+            {socketRespons.map((msg: any) => (
+              <li
+                key={msg.id}
+              >{`[${msg.timeStamp}][${msg.user}]: ${msg.text}`}</li>
+            ))}
+          </ul>
+        </section>
+      </div>
+    </>
+  );
 }
