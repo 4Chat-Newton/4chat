f1903f8951d51e1895834f471d58d852493b90d1
M0ller <simon.mollerberg@gmail.com>
2023-02-10 23:50:18 +0100

    cypress test works locally

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -1,12 +1,12 @@
 import Navbar from "../components/globalComponents/Navbar"
 import Chat from "../components/chat/Chat"
 import BoxContainer from "../components/globalComponents/BoxContainer"
-import InboxTab from "../components/rightComponent/tabs/InboxTab";
+import Inbox from "../components/rightComponent/Inbox";
 export default function ChatRoom() {
     return <>
         <Navbar />
         <BoxContainer className="chatBox">
-        <InboxTab />
+        <Inbox />
         <Chat />
         </BoxContainer>
     </>
