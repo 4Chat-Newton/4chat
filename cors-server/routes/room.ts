5ffcfe18654d90fe578a7d45d58a7354f9acb87d
Liha86 <sanjin.elfynd@gmail.com>
2023-02-10 08:40:22 +0100

    added changes to deleteRoom , WIP

--- cors-server/routes/room.ts
+++ cors-server/routes/room.ts
@@ -1,92 +1,77 @@
 import { requireSignin } from "../controllers/authentication";
 import express, { response, request } from 'express';
 
-export const findExistingRoom = async function (name, db) {
-    try {
-        const result = await db
-            .prepare(
-                "SELECT name FROM room WHERE name = ?"
-            )
-            .get(name);
 
-        if (!result) {
-            console.log(`No room found with the following name: ${name}`);
-            return null;
-        }
-        return result;
-    } catch (error) {
-        console.error(`${error}`)
-        return null;
+export const findExistingRoom = async function (name, db) {
+  try {
+    const result = await db
+      .prepare(
+        "SELECT name FROM room WHERE name = ?"
+      )
+      .get(name);
+
+    if (!result) {
+      console.log(`No room found with the following name: ${name}`);
+      return null;
     }
+    return result;
+  } catch (error) {
+    console.error(`${error}`)
+    return null;
+  }
 }
 
 export const createRoom = async function (server, db) {
-    server.post("/data/room", async (req: express.Request, res: express.Response) => {
-        const {id, status} = requireSignin(req, res);
-        const { name } = req.body;
-        const existingRoom = await findExistingRoom(name, db)
+  server.post("/data/room", async (req: express.Request, res: express.Response) => {
+    const { id, status } = requireSignin(req, res);
+    const { name } = req.body;
+    const existingRoom = await findExistingRoom(name, db)
 
-        if (status) {
-            try {
-
-                if (!existingRoom) {
-
-                    await db.prepare("INSERT INTO room (creator_id, name) VALUES(?,?)").run(id, name);
-                    return res.status(200).json({msg: "Room created"})
-                } else {
-                    return res.status(400).json({error: `Room "${name}" already exists!`})
-                }
-            } catch (e) {
-                return res
-                    .status(400)
-                    .json({ error: "Failed to create room" });
-            }
-        } else {
-            return res
-                .status(400)
-                .json({ error: "Invalid Credentials!" });
-        }
-    })
-}
-
-export const getAllRooms = async function (server, db){
-    server.get("/data/room", async (req: express.Request, res: express.Response) => {
-        try {
-            const result = await db
-                .prepare(
-                    "SELECT * FROM room"
-                )
-                .get();
-            return res.status(200).send(result)
-        } catch(e) {
-            return res.status(400).send("Failed to retrieve rooms!")
-        }
-    })
-}
-
-export const deleteRoom = async function (server, db){
-  server.delete("/data/room", async (req: express.Request, res: express.Response) => {
-    const existingRoom = await findExistingRoom(req.body.name, db)
-    if (!existingRoom) {
-      return res.status(400).send(`Room '${req.body.name}' doesn't exist!`)
-    } else {
+    if (status) {
       try {
-        await db.prepare("DELETE FROM room WHERE name = ?").run(req.body.name);
-        return res.status(200).send(`Room '${req.body.name}' has been deleted!`)
-      } catch(e) {
-        return res.status(400).send(`Failed to delete room '${req.body.name}'!`)
+
+        if (!existingRoom) {
+
+          await db.prepare("INSERT INTO room (creator_id, name) VALUES(?,?)").run(id, name);
+          return res.status(200).json({ msg: "Room created" })
+        } else {
+          return res.status(400).json({ error: `Room "${name}" already exists!` })
+        }
+      } catch (e) {
+        return res
+          .status(400)
+          .json({ error: "Failed to create room" });
       }
+    } else {
+      return res
+        .status(400)
+        .json({ error: "Invalid Credentials!" });
     }
   })
 }
 
-export const leaveChatRoom =async (server ,db) => {
+export const getAllRooms = async function (server, db) {
+  server.get("/data/room", async (req: express.Request, res: express.Response) => {
+    try {
+      const result = await db
+        .prepare(
+          "SELECT * FROM room"
+        )
+        .get();
+      return res.status(200).send(result)
+    } catch (e) {
+      return res.status(400).send("Failed to retrieve rooms!")
+    }
+  })
+}
+
+export const leaveChatRoom = async (server, db) => {
   server.delete("/data/room/leave", async (req: express.Request, res: express.Response) => {
     const { id } = requireSignin(req, res);
     const { room_id } = req.body;
     const roomCheck = await db.prepare("SELECT * FROM joined_room WHERE user_id = ? AND room_id = ?").all(id, room_id);
 
-    if (!roomCheck || !roomCheck.user_id || !roomCheck.room_id)  {
+    if (!roomCheck || !roomCheck.user_id || !roomCheck.room_id) {
       return res.status(400).send(`Room '${req.body.room_id}' doesn't exist!`)
     } else {
       try {
@@ -98,4 +83,39 @@ export const leaveChatRoom =async (server ,db) => {
     }
   })
 }
+
+
+export const deleteRoom = async function (server, db) {
+  server.delete("/data/room", async (req: express.Request, res: express.Response) => {
+    try {
+      const existingRoom = await findExistingRoom(req.body.name, db);
+      const { id, status } = requireSignin(req, res);
+      const { creator_id, name } = req.body;
+      console.log(creator_id)
+
+      if (!status) {
+        return res.status(401).send({ error: "Unauthorized" });
+      }
+
+      if (!existingRoom) {
+        return res.status(400).send({ error: `Room '${name}' does not exist` });
+      }
+
+      if (!creator_id) {
+        return res.status(400).send({ error: "Missing creator_id in the request body" });
+      }
+
+      if (id !== creator_id) {
+        return res.status(403).send({ error: "Forbidden" });
+      }
+
+      await db.prepare("DELETE FROM room WHERE name = ?").run(name);
+      return res.status(200).send({ message: `Room '${name}' has been deleted` });
+    } catch (error) {
+      return res.status(500).send({ error: "Internal Server Error" });
+    }
+  });
+};
+
+
 
