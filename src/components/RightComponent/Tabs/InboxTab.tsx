4e3c1b6ccfb240cfa258b3958ddf352d5c6f89af
M0ller <simon.mollerberg@gmail.com>
2023-02-11 14:04:03 +0100

    testing to uncomment some code

---
+++ src/components/RightComponent/Tabs/InboxTab.tsx
@@ -0,0 +1,13 @@
+import TabsComponent from "../globalComponents/TabsComponent";
+
+function InboxTab(){
+
+    return (
+        <TabsComponent className="InboxTab">
+            <h2>DM</h2>
+            Establish database connection 
+        </TabsComponent>
+    );
+};
+
+export default InboxTab
