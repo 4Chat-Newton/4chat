f69729b61d47092ce28b8a7628858a2781217084
M0ller <simon.mollerberg@gmail.com>
2023-02-12 13:45:01 +0100

    new

---
+++ src/components/RightComponent/tabs/InboxTab.tsx
@@ -0,0 +1,12 @@
+import TabComponent from "../../GlobalComponents/TabComponent";
+
+const InboxTab = () =>{
+
+    return (
+        <TabComponent className="InboxTab">
+            <h2>DM</h2>
+            Establish database connection 
+        </TabComponent>
+    );
+};
+export default InboxTab
