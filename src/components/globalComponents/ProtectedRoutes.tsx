67517c094408e1fdcfc5f3c6e6c264f8c8709264
Hannes Westin <hannes.westin@edu.newton.se>
2023-02-13 11:48:56 +0100

    Protected routes functional

--- src/components/globalComponents/ProtectedRoutes.tsx
+++ src/components/globalComponents/ProtectedRoutes.tsx
@@ -1,25 +1,18 @@
-import { response } from "express";
 import { Outlet, Navigate } from "react-router-dom";
 
 const ProtectedRoutes = () => {
-    //TODO actual logic for handling tokens
-    const jwtToken = localStorage.getItem('token') //this could work if we use localstorage
-    // const getToken = async () => {
-    //     await fetch('http://localhost:8080/data/login', {
-    //         method: 'GET',
-    //         headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${jwtToken}` },
-    //     }).then(response => {
-    //         if(jwtToken !==null){
-    //             return true
-    //         } else {
-    //             return false
-    //         }
-    //     })
-    // }
 
-    let auth = { jwtToken }
+    const getToken = () => {
+        const jwtToken = localStorage.getItem('token');
+        if (jwtToken !== null) {
+            return true
+        } else {
+            return false
+        }
+    }
+
     return (
-        auth ? <Outlet /> : <Navigate to="/" />
+        getToken() ? <Outlet /> : <Navigate to="/" />
     )
 };
 
