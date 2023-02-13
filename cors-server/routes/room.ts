fa6bdcf5b902a7737121086dbc57af1331527a7d
Liha86 <sanjin.elfynd@gmail.com>
2023-02-13 13:18:29 +0100

    Added unuique creation for url

--- cors-server/routes/room.ts
+++ cors-server/routes/room.ts
@@ -34,7 +34,6 @@ export const createRoom = async function (server, db) {
           await db.prepare("INSERT INTO room (creator_id, name) VALUES(?,?)").run(result.id, name);
           const room = await db.prepare(`SELECT * FROM room WHERE name = '${name}'`).get();
           const roomUrl = `/data/room/${room.name}`;
-          console.log(roomUrl);
           return res.status(200).json({msg: `Room created`, roomUrl})
         } else {
           return res.status(400).json({error: `Room "${name}" already exists!`})
