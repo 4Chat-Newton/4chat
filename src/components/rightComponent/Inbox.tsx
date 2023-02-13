f8ec1e109ceeba729fa03a3d3d889894e134f9e7
M0ller <simon.mollerberg@gmail.com>
2023-02-13 09:46:14 +0100

    hide inboxtab

--- src/components/rightComponent/Inbox.tsx
+++ src/components/rightComponent/Inbox.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./Inbox.css";
-import InboxTab from "./tabs/InboxTab";
 import RoomTab from "./tabs/RoomTab";
+// import InboxTab from "./tabs/InboxTab";
 
 export default function Inbox() {
 
@@ -20,11 +20,12 @@ export default function Inbox() {
         <>
             <section className="inbox">
                 <ul className="inboxBtns">
-                    <li className={activeTab === "inbox" ? "active" : ""} onClick={handleInbox}>Inbox</li>
+                    {/*<li className={activeTab === "inbox" ? "active" : ""} onClick={handleInbox}>Inbox</li>*/}
                     <li className={activeTab === "rooms" ? "active" : ""} onClick={handleRoom}>Rooms</li>
                 </ul>
                 <div className="outlet">
-                    {activeTab === "inbox" ? <InboxTab /> : <RoomTab />}
+                    {/*{activeTab === "inbox" ? <InboxTab /> : <RoomTab />}*/}
+                    { < RoomTab />}
                 </div>
             </section>
         </>
