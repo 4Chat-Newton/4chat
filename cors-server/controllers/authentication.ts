d138418600711743a2805c22e6eacf1462dd7bc2
M0ller <simon.mollerberg@gmail.com>
2023-02-15 18:06:29 +0100

    storing message with post

--- cors-server/controllers/authentication.ts
+++ cors-server/controllers/authentication.ts
@@ -72,6 +72,9 @@ export const verifyJWT = (req: express.Request, res: express.Response, next) => 
     })
     let bearerToken = req.headers['authorization']
     let token  = bearerToken.replace("Bearer ", "");
+
+    console.log("token: ", bearerToken)
+
     jwt.verify(token, "secret_key")
 
     if (!token) {
--- cors-server/routes/message.ts
+++ cors-server/routes/message.ts
@@ -5,17 +5,28 @@ export const storeMessage = async (server, db) => {
 
     server.post("/data/message", verifyJWT, async (req: express.Request, res: express.Response) => {
         let user = returnUser(req, res);
-        const {sender_id, receiver_id, room, message, timestamp} = req.body;
-        console.log("user.id: ",user.id, "receiver_id: ", receiver_id, "room: ", room,"message: ", message)
+        // let body = {sender_id: Number,  receiver_id: Number, room: Boolean, message: String, timestamp: Date}
+        let  {sender_id, receiver_id, room, message, timestamp} = req.body;
+        // body = req.body;
+        console.log("user.id: ",user.id)
+        // console.log("receiver_id: ", receiver_id.typeof)
+        console.log("receiver_id: ", receiver_id)
+        // console.log("room: ", room.typeof)
+        console.log("room: ", room)
+        // console.log("message: ", body.message.typeof)
+        console.log("message: ", message)
 
         try {
-            await db.prepare("INSERT INTO message (sender_id, receiver_id, room, message) VALUES(?,?,?,?)")
-                .run(user.id, receiver_id, room, message);
-            return res.status(200).json({msg: "Stored message successfully!"})
+            // const result = await db.prepare("INSERT INTO message (sender_id, receiver_id, room, message) VALUES(?,?,?,?)")
+            //     .run(user.id, receiver_id, room, message);
+            const result = await db.prepare("INSERT INTO message (sender_id, receiver_id, room, message, timestamp) VALUES(?, ?, ?, ?, ?)")
+                .run(user.id, receiver_id, room, message, timestamp);
+            console.log("result: ", result)
+            return res.status(200).json({message: "Stored message successfully!"})
         } catch (e) {
             return res
                 .status(400)
-                .json({error: "Failed to store message"});
+                .json({message : "Failed to store message", error: e});
         }
 
     })
