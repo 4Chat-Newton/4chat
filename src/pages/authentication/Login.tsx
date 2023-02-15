a315adfbef8dcb6616de00bcc31c83fcf5bb6df8
Hannes Westin <hannes.westin@edu.newton.se>
2023-02-15 20:10:38 +0100

    login test

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -1,5 +1,6 @@
 import { useState} from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {API_BASE_URL} from "../../consts"
 
 export default function Login() {
 
@@ -24,7 +25,7 @@ export default function Login() {
     }
     const signIn = async () => {
         //TODO fetch should be '/data/login'
-        await fetch('http://localhost:8080/data/login', {
+        await fetch(`${API_BASE_URL}/data/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
