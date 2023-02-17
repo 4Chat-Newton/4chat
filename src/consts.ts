a93d0a3620316c20861a910006b82b336230d7d0
Simon M <65451384+M0ller@users.noreply.github.com>
2023-02-17 08:39:24 +0000

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.NODE_ENV === 'developement' ? 'http://localhost:8080': ''
+export const API_BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080': ''
